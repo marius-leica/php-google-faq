4a0ce01c498b54852129a0f547d84bbda3d65732
Marius L <mariusleica@gmail.com>
2022-07-11 15:39:24 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/* Riscrivere questa pagina del sito google
+https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte.
+Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP. */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="682665_favicon_google_logo_new_icon.ico" rel="icon" type="image/x-icon" />
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css' integrity='sha512-GQGU0fMMi238uA+a/bdWJfpUGKUkBdgfFdgBm72SUQ6BeyWjoY/ton0tEjH+OSH9iP4Dfh+7HM0I9f5eR0L/4w==' crossorigin='anonymous' />
+    <title>Google FAQ</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
